ff3f6fa0c46548ef9b29f18d7390c3cb20eb3411
Felisong <carolinahs100@gmail.com>
2024-11-11 10:45:50 -0700

    basic construction of code layout written

---
+++ calculator.js
@@ -0,0 +1,14 @@
+// Variables at the top
+// VARIABLES
+
+// something to convert the variables into floats
+
+//OPERATORS OBJECT
+
+// console.log instructions ie.
+// please input your question, following the structure of "node calculator.js 24 * 3"
+
+// console.log to show result variable add .toFixed(2)
+
+// all functions bottom
+// FUNCTIONS
